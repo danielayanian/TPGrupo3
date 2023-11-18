ea2bf7bd0690a87b0df1f1bd3b13af35ace8a37d
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2023-11-18 03:13:59 -0300

    creacion del proyecto

---
+++ src/main/java/ap/grupo3/tpgrupo3/TpGrupo3Application.java
@@ -0,0 +1,13 @@
+package ap.grupo3.tpgrupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGrupo3Application.class, args);
+    }
+
+}
